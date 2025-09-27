48a3b43994cb4ce155d0d4ad37977eb20b13c93b
GitCoder820 <programpro0001@outlook.com>
2025-09-28 04:12:34 +0530

    page selector

---
+++ src/components/left_pannel/current.js
@@ -0,0 +1,8 @@
+let current=""
+
+export function setCurrent(next) {
+  currnet=next;
+}
+export function getCurrent() { 
+  return current;
+}
--- src/components/left_pannel/explorer.jsx
+++ src/components/left_pannel/explorer.jsx
@@ -1,39 +1,49 @@
 import { NavLink } from "react-router-dom"
+import { setCurrent } from "./current"
+import { getCurrent } from "./current"
 import styles from  "./explorer.module.css"
 import home from "./asset/home.svg"
 import trending from "./asset/trending.svg"
 import you from "./asset/you.svg"
 import upload from "./asset/upload.svg"
 import like from "./asset/like.svg"
+import { useEffect, useState } from "react"
 export default function () {
+    const [red,setRed]=useState("red")
+    useEffect(() => {
+    const el = document.getElementById(getCurrent());
+    if (el) {
+      el.style.backgroundColor = "red";
+    }
+  }, []);
     return (<>
         <ul className={styles.ul}>
             <li>
-                <NavLink to='/home'><div className="styles.links">
+                <NavLink to='/home' className={({ isActive }) =>isActive ? `${styles.activeLink}` : ""}><div className="styles.links">
                     <img className={styles.icons} src={home} alt="menu" /> <div className={styles.hidden}>Home</div>
                 </div></NavLink>
                 
             </li>
             <li>
-                <NavLink to='/trending'><div className="styles.links">
+                <NavLink to='/trending' className={({ isActive }) =>isActive ? `${styles.activeLink}` : ""}><div className="styles.links">
                     <img className={styles.icons} src={trending} alt="menu" /> <div className={styles.hidden}>Trending</div>
                 </div></NavLink>
                 
             </li>
             <li>
-                <NavLink to='/upload'><div className="styles.links">
+                <NavLink to='/upload'  className={({ isActive }) =>isActive ? `${styles.activeLink}` : ""} ><div className="styles.links">
                     <img style={{background:"black",borderRadius:"3.14rem"}} className={styles.icons} src={upload} alt="menu" /> <div className={styles.hidden}>Upload</div>
                 </div></NavLink>
                 
             </li>
             <li>
-                <NavLink to='/library'><div className="styles.links">
+                <NavLink to='/library' className={({ isActive }) =>isActive ? `${styles.activeLink}` : ""}><div className="styles.links">
                     <img style={{background:"black",borderRadius:"3.14rem"}} className={styles.icons} src={you} alt="menu" /> <div className={styles.hidden}>You</div>
                 </div></NavLink>
                 
             </li>
             <li>
-                <NavLink to='/history'><div className="styles.links">
+                <NavLink to='/history' className={({ isActive }) =>isActive ? `${styles.activeLink}` : ""}><div className="styles.links">
                     <img  className={styles.icons} src={like} alt="menu" /> <div className={styles.hidden}>Like</div>
                 </div></NavLink>
                 
