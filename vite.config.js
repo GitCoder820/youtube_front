e771cbe11266a952c912a40f68015e80a08d140d
GitCoder820 <programpro0001@outlook.com>
2026-04-18 18:21:06 +0530

    project ends

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000/",
+        target: "https://youtube-backend-8o8a.onrender.com/",
         changeOrigin: true,
         secure: true,
         rewrite: (path) => path.replace(/^\/api/, "")
